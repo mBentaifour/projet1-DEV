5f11fa98c90cb3a52f2af838b2a1a7a485051f7c
mbentaifour <bentaifourmoh@gmail.com>
2025-07-31 16:26:59 +0200

    Initialisation du backend Django

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # Tiers
     'rest_framework',
+    'rest_framework_simplejwt',
 
     # Apps perso
     'users',
@@ -126,3 +127,16 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+from datetime import timedelta
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+}
